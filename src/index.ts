8e30ae9eb594620ca01f6bfc74c1c243bae64225
Ameth <Amethgm@gmail.com>
2024-02-21 17:39:40 -0500

    perf: :zap: Mejoras a las peticiones
    se mejoran las peticiones y se crea un archivo nuevo para el modelo

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,20 @@ async function main() {
         `https://tiendasisimo.com/mercado/?product-page=${i}`
       );
       const $ = cheerio.load(response.data);
+
+      //Array  que guarda los productos de la página actual
       const products = $(".product");
 
       for (let element of products.toArray()) {
+        //Se toma los datos de cada producto
         const name = $(element).find(".woocommerce-loop-product__title").text();
         const image =
           $(element).find(".size-woocommerce_thumbnail").attr("data-src") ||
           $(element).find(".size-woocommerce_thumbnail").attr("src");
         const state = getRandomState();
-        const ProductData = await getProductData(name);
+
+        //Se busca cada productos de forma individual en la pagina para agregar a la nueva variable
+        let ProductData = await getProductData(name);
 
         if (ProductData) {
           content.push({
@@ -30,17 +35,12 @@ async function main() {
             state,
             Product: ProductData,
           });
-        } else {
-          // Si getProductData no devuelve nada, aún puedes decidir agregar el producto sin los datos adicionales
-          content.push({ name, image, state });
         }
       }
-      console.log(`PÁGINA [${i}] PRODUCTOS ESCANEADOS`);
+      console.log(`PÁGINA [${i}/${number}] PRODUCTOS ESCANEADOS`);
     }
 
-    // await addToJSON(content); // Asumiendo que quieres agregar todo el contenido al JSON
-    // Si setJSON debe usarse en lugar de addToJSON, asegúrate de que cumple con lo que necesitas
-
+    //Save JSON File
     setJSON(content);
   } catch (error) {
     console.error("Error en la ejecución:", error);
